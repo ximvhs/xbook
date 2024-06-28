0598731052721c69a894d40f20ed7c4fbe939a35
ximvhs <ximvhs26092002@gmail.com>
2024-06-28 10:20:35 +0700

    update

--- general/general.component.ts
+++ general/general.component.ts
@@ -8,6 +8,7 @@ import { MedicalRecordService } from "../medical-record.service";
 import { BENHSU, LISTPATIENT } from "../mock.service";
 import { BenhSu, listPatient } from "../model";
 import { HeroService } from "../hero.service";
+import { groupBy } from "lodash";
 
 @Component({
   selector: "xb-general",
@@ -19,6 +20,11 @@ export class GeneralComponent implements OnInit {
   clientList: listPatient[] = [...LISTPATIENT];
   benhSu: BenhSu = { ...BENHSU };
   client: any;
+  trieuChungTH = [];
+  chuanDoanTH = [];
+  xetNghiemTH = [];
+  thuocDieuTriTH = [];
+  canLamSangTH = [];
 
   constructor(
     private router: Router,
@@ -30,7 +36,16 @@ export class GeneralComponent implements OnInit {
   isOpenInfo = false;
   isOpenBA = false;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.heroService.mergeAndFilterChiTiet().subscribe((data) => {
+      const groupedData = groupBy(data, "loaiXetNghiem");
+      this.trieuChungTH = groupedData.trieuChung;
+      this.chuanDoanTH = groupedData.chuanDoan;
+      this.xetNghiemTH = groupedData.xetNghiem;
+      this.thuocDieuTriTH = groupedData.thuocDieuTri;
+      this.canLamSangTH = groupedData.canLamSang;
+    });
+  }
 
   toggle() {
     this.isOpenInfo = !this.isOpenInfo;
@@ -41,6 +56,38 @@ export class GeneralComponent implements OnInit {
   }
   navigateDetail(id: number) {
     this.router.navigate([`/pages/medic/detail/${id}`]);
+  }
+
+  images = [
+    "https://ykhoahopnhan.vn/wp-content/uploads/2018/02/1.png",
+    "https://medlatec.vn/media/8114/content/20211210_xet-nghiem-la-gi-01.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfsmKrlim1WHI_GR9ggfV66aBx5E7n6xinIw&s",
+  ];
+
+  captions = ["Máu - 26/06/2024", "Tay - 26/06/2024", "Chân - 26/06/2024"];
+
+  currentIndex = 0;
+  isFullScreen = false;
+  isSearchComplete = true;
+
+  prevSlide() {
+    this.currentIndex =
+      this.currentIndex > 0 ? this.currentIndex - 1 : this.images.length - 1;
+  }
+
+  nextSlide() {
+    this.currentIndex =
+      this.currentIndex < this.images.length - 1 ? this.currentIndex + 1 : 0;
+  }
+
+  toggleFullScreen() {
+    this.isFullScreen = !this.isFullScreen;
+  }
+
+  search() {
+    // Thực hiện logic tìm kiếm của bạn ở đây
+    // Khi tìm kiếm hoàn tất, đặt isSearchComplete thành true
+    this.isSearchComplete = true;
   }
 
   // searchClient = (text$: Observable<string>) => {
